31b82caa09e0101270f30293f098da027933582c
Sauce77 <sauceortediego505@gmail.com>
2025-05-02 11:41:40 -0600

    se agrego todos totales

---
+++ scripts/excel_formats.py
@@ -0,0 +1,7 @@
+import xlsxwriter
+
+def totales_format_excel():
+    """
+        Convierte la informacion JSON de totales 
+    """
+    return 0
--- totales.py
+++ totales.py
@@ -9,9 +9,41 @@ routes_totales = Blueprint("totales", __name__)
 API_URL_EXTRACCION = "https://grc-api.onrender.com/extraccion/"
 API_URL_CERTIFICACION = "https://grc-api.onrender.com/certificacion/"
 
-@routes_totales.route("/<app>", methods=["GET","POST"])
-def root(app):
+@routes_totales.route("/", methods=["GET", "POST"])
+def mostrar_todos_totales():
+    """
+        Muestra totales de todas las aplicaciones.
+    """
+    auth = session.get("user")
 
+    if not auth:
+        return redirect(url_for('login_user'))
+    
+    # cabecera utilizada para la peticion
+    headers = {
+        'Authorization': f'Token {auth["token"]}'
+    }
+
+    # peticion para registros
+    url = API_URL_EXTRACCION + "registros/"
+    respuesta = requests.get(url, headers=headers)
+    respuesta.raise_for_status()  # Lanza una excepción para códigos de error 4xx o 5xx
+    registros_json = respuesta.json()
+
+    # peticion para aplicativos
+    url = API_URL_EXTRACCION + "apps"
+    respuesta = requests.get(url, headers=headers)
+    respuesta.raise_for_status()  # Lanza una excepción para códigos de error 4xx o 5xx
+    apps_json = respuesta.json()
+
+    json_total_app, json_totales_res = obtener_totales(registros_json)
+    return render_template("totales/mostrar_totales.html",auth=auth,apps=apps_json,total_app=json_total_app,totales=json_totales_res,app="todos los Aplicativos")
+
+@routes_totales.route("/<app>", methods=["GET","POST"])
+def mostrar_app_totales(app):
+    """
+        Muestra totales de la aplicacion especifica.
+    """
     auth = session.get("user")
 
     if not auth:
