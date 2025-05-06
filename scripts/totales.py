f56d5676f4791bcc39939205a15f59d19eaf747e
Sauce77 <sauceortediego505@gmail.com>
2025-05-06 17:38:50 -0600

    se creo formato totales

--- scripts/totales.py
+++ scripts/totales.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import xlsxwriter
+from xlsxwriter.utility import cell_autofit_width
 import io
 
-def obtener_totales(datos):
+def obtener_totales(df):
     """
         Recibe como entrada los registros de un app en formato JSON,
         utiliza pandas para obtener la cantidad de usuarios por responsable.
     """
-    df = pd.json_normalize(datos)
-
     # obtenemos totales de aplicacion
 
     # obtenemos el numero de usuarios a certificar
@@ -75,21 +74,82 @@ def obtener_totales(datos):
 
     return total_app, total_responsables
 
+def formato_excel(hoja, formatos, total_app, totales_responsables):
+    """
+        Ingresa los totales de la aplicacion en un formato
+        de la hoja elegida.
+    """
+    
+    ancho = 0
+    for index,(clave, valor) in enumerate(total_app.items()):
 
-def obtener_totales_excel(datos):
+        # para ajustar el ancho de columna clave
+        if len(clave) > ancho:
+            ancho = len(clave)
+
+        # titulo sin guion bajo y en mayusculas
+        titulo = clave.replace("_"," ").upper()
+
+        # ingresamos titulo
+        hoja.write(index, 0, titulo, formatos["titulo"])
+        # ingresamos cifra
+        hoja.write(index, 2, valor, formatos["cifra"])
+        index = index + 1
+
+    # ajustamos ancho de columna titulos
+    hoja.set_column(0,0,ancho+7)
+
+    # agregamos el encabezado
+    for index in range(4):
+        hoja.write(6, index, "", formatos["encabezado"])
+    hoja.write_string(6, 0, "Responsable", formatos["encabezado"])
+
+    # agrega resultados por responsable
+
+    # obtenemos el numero de resultados por responsable
+    filas_responsable = len(totales_responsables[0])
+
+    for index,total in enumerate(totales_responsables):
+
+        hoja.write(8+(index*filas_responsable), 0, total["responsable"], formatos["texto"])
+        for index_fila, (clave, valor) in enumerate(total.items()):
+
+            if clave == "responsable":
+                continue
+
+            hoja.write(8+(index*filas_responsable)+index_fila-1, 1, valor, formatos["texto"])
+
+def obtener_totales_excel(registros_json):
     """
         Obtiene todos los registros de aplicativos y divide para cada uno
         los totales en un archivo excel.
     """
-    df = pd.json_normalize(datos)
+    df_registros = pd.json_normalize(registros_json)
 
     # para escribir en a memoria
     output = io.BytesIO()
     # creamos el workbook
     libro = xlsxwriter.Workbook(output)
 
-    hoja = libro.add_worksheet("Euphories")
-    hoja.write("A1", "Pourrions-nous revenir sur cette histoire?")
+    formatos = {
+        "titulo": libro.add_format({'bold':True, 'bg_color': '#E8E8E8', 'font_size': 12}),
+        "encabezado": libro.add_format({'bg_color': '#000000', 'color': '#FFFFFF', 'font_size': 14}),
+        "cifra": libro.add_format({'bg_color': '#B4CBFB', 'border': 1, 'font_size': 12}),
+        "texto": libro.add_format({'font_size': 11})
+    }
+
+    # Se registran los totales de toda la extraccion
+    hoja = libro.add_worksheet("Extraccion")
+
+    totales_todo, totales_todo_responsable = obtener_totales(df_registros)
+
+    formato_excel(hoja, formatos, totales_todo, totales_todo_responsable)
+
+    
+    
+    # obtenemos el nombre de las apps
+    apps = df_registros["app"].unique()
+
     libro.close()
     # volvemos al inicio de la memoria
     output.seek(0)
--- totales.py
+++ totales.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from flask import Blueprint,render_template,request, session, url_for, send_file, redirect
+import pandas as pd
 import io
 import xlsxwriter
 
@@ -38,7 +39,10 @@ def mostrar_todos_totales():
     respuesta.raise_for_status()  # Lanza una excepción para códigos de error 4xx o 5xx
     apps_json = respuesta.json()
 
-    json_total_app, json_totales_res = obtener_totales(registros_json)
+    # convertimos los registros a un dataframe
+    df_registros = pd.json_normalize(registros_json)
+
+    json_total_app, json_totales_res = obtener_totales(df_registros)
         
     return render_template("totales/mostrar_totales.html",auth=auth,apps=apps_json,total_app=json_total_app,totales=json_totales_res,app="todos los Aplicativos")
 
@@ -69,7 +73,10 @@ def mostrar_app_totales(app):
     respuesta.raise_for_status()  # Lanza una excepción para códigos de error 4xx o 5xx
     apps_json = respuesta.json()
 
-    json_total_app, json_totales_res = obtener_totales(registros_json)
+    # convertimos los registros a un dataframe
+    df_registros = pd.json_normalize(registros_json)
+
+    json_total_app, json_totales_res = obtener_totales(df_registros)
     return render_template("totales/mostrar_totales.html",auth=auth,apps=apps_json,total_app=json_total_app,totales=json_totales_res,app=app)
 
 
@@ -79,17 +86,33 @@ def descargar_excel():
         Convierte la informacion JSON de totales 
     """
 
+    auth = session.get("user")
+
+    if not auth:
+        return redirect(url_for('login_user'))
+    
+    # cabecera utilizada para la peticion
+    headers = {
+        'Authorization': f'Token {auth["token"]}'
+    }
+
+    # peticion para registros
+    url = API_URL_EXTRACCION + "registros/"
+    respuesta = requests.get(url, headers=headers)
+    respuesta.raise_for_status()  # Lanza una excepción para códigos de error 4xx o 5xx
+    registros_json = respuesta.json()
+
     # guardamos el archivo a generar en memoria
     output = io.BytesIO()
 
     try:
-        output = obtener_totales_excel({"prenom": "euphories"})
-        
+        output = obtener_totales_excel(registros_json)
+            
         return send_file(
             output,
             mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
             as_attachment=True,
-            download_name='mi_archivo_generado.xlsx'
+            download_name='Totales.xlsx'
         )
 
     except Exception as e:
